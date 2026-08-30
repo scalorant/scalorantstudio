9637a1395402ca8df995508be03ffdd4ba29b350
scalorant <scalorant@gmail.com>
2026-08-30 12:22:46 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/scalorantstudio",
+  assetPrefix: "/scalorantstudio/",
   images: {
     unoptimized: true,
   },
